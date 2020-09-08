98c794fc152fe54f176bf81d1355bb9d475e6f53
Ján Vdovec <jan.vdovec@gmail.com>
2020-09-09 01:36:38 +0200

    Bind MediaConversionServiceInterface to concrete ElementalMediaConvertService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App;
+use App\Services\AmazonWebServices\ElementalMediaConvertService;
+use App\Services\MediaConversionServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        App::bind( MediaConversionServiceInterface::class,  ElementalMediaConvertService::class );
     }
 
     /**
