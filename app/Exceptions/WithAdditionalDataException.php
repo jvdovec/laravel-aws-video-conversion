cf29a9184b641d989ec7d2617431ee91d611fac3
Ján Vdovec <jan.vdovec@gmail.com>
2020-09-09 01:30:22 +0200

    Add WithAdditionalDataException

---
+++ app/Exceptions/WithAdditionalDataException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class WithAdditionalDataException extends Exception
+{
+    protected array $_data;
+
+    public function __construct(string $message = '', array $data = [])
+    {
+        $this->_data = $data;
+        parent::__construct($message);
+    }
+
+    public function getData() : array
+    {
+        return $this->_data;
+    }
+}
