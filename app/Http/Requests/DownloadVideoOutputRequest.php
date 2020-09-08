2af6d075a0a6d4255a89040bc29303d6eca3fa89
Ján Vdovec <jan.vdovec@gmail.com>
2020-09-09 01:35:35 +0200

    Add request classes
    DownloadVideoOutputRequest
    DownloadVideoThumbnailRequest

---
+++ app/Http/Requests/DownloadVideoOutputRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DownloadVideoOutputRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'fileKey' => 'required'
+        ];
+    }
+
+    public function getFileKey()
+    {
+        $validated = $this->validated();
+        return $validated['fileKey'] ?? null;
+    }
+}
