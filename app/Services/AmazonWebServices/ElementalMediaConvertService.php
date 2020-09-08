e256169660138e30121cc686a5b02553040fea83
Ján Vdovec <jan.vdovec@gmail.com>
2020-09-09 01:31:44 +0200

    Add implementation of AWS MediaConvert

---
+++ app/Services/AmazonWebServices/ElementalMediaConvertService.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Services\AmazonWebServices;
+
+use App\Exceptions\WithAdditionalDataException;
+use App\Services\MediaConversionServiceInterface;
+use App\Types\AmazonWebServices\ConversionSettingsType;
+use Aws\MediaConvert\MediaConvertClient;
+use Aws\Result;
+use Cache;
+use Exception;
+
+class ElementalMediaConvertService implements MediaConversionServiceInterface
+{
+    protected ?MediaConvertClient $client;
+
+    /**
+     * @throws WithAdditionalDataException
+     */
+    public function __construct()
+    {
+        $singleEndpointUrl = $this->getSingleEndpointUrl();
+
+        try {
+            $this->client = new MediaConvertClient([
+                'version' => config('aws.mediaconvert.client_version'),
+                'region' => config('aws.mediaconvert.region'),
+                'endpoint' => $singleEndpointUrl,
+            ]);
+        } catch (Exception $e) {
+            throw new WithAdditionalDataException('Problem with initialising convert client', ['exception' => $e]);
+        }
+    }
+
+
+    /**
+     * @throws WithAdditionalDataException
+     */
+    protected function getSingleEndpointUrl()
+    {
+        $cacheKey = 'aws_mediaconvert_endpoint';
+
+        if (Cache::has($cacheKey)) {
+            return (Cache::get($cacheKey));
+        }
+
+        try {
+
+            $client = new MediaConvertClient([
+                'version' => config('aws.mediaconvert.client_version'),
+                'region' => config('aws.mediaconvert.region'),
+            ]);
+
+            $result = $client->describeEndpoints([]);
+            $singleEndpointUrl = $result['Endpoints'][0]['Url'];
+
+            // Cache endpoint url for approx. one month
+            Cache::put($cacheKey, $singleEndpointUrl, 86400 * 31);
+
+            return $singleEndpointUrl;
+
+        } catch(Exception $e) {
+            throw new WithAdditionalDataException('Problem with getting single endpoint', ['exception' => $e]);
+        }
+    }
+
+    /**
+     * @param ConversionSettingsType $conversionSettings
+     * @return Result
+     * @throws WithAdditionalDataException
+     */
+    protected function createJob(ConversionSettingsType $conversionSettings)
+    {
+        $parameters = [
+            'Role' => config('aws.mediaconvert.iam_role_arn'),
+            'Settings' => $conversionSettings->getAll(),
+            'Queue' => config('aws.mediaconvert.queue_arn'),
+            'UserMetadata' => [
+                'Customer' => 'Amazon'
+            ],
+        ];
+
+        try {
+            return $this->client->createJob($parameters);
+        }
+        catch (Exception $e)
+        {
+            throw new WithAdditionalDataException('There was problem with creating job', ['exception' => $e]);
+        }
+    }
+
+    /**
+     * @param string $videoInputFullyQualifiedPathWithExtension
+     * @param string $videoOutputFullyQualifiedPathWithoutExtension
+     * @param string $videoThumbnailsFullyQualifiedPath
+     * @return string
+     * @throws WithAdditionalDataException
+     */
+    public function queueConversion(string $videoInputFullyQualifiedPathWithExtension, string $videoOutputFullyQualifiedPathWithoutExtension, string $videoThumbnailsFullyQualifiedPath) : string
+    {
+        $conversionSettings = new ConversionSettingsType($videoInputFullyQualifiedPathWithExtension, $videoOutputFullyQualifiedPathWithoutExtension, $videoThumbnailsFullyQualifiedPath);
+
+        $response = $this->createJob($conversionSettings);
+        if (!$response) {
+            throw new WithAdditionalDataException('Get no response when queuing conversion', ['response' => $response]);
+        }
+
+        $jobData = $response->get('Job');
+
+        if (!isset($jobData['Id'])) {
+            throw new WithAdditionalDataException('Could not get ID of the job', ['response' => $response]);
+        }
+
+        return $jobData['Id'];
+    }
+
+
+    /**
+     * @param string $conversionJobId
+     * @return array
+     * @throws Exception
+     */
+    public function getConversionJobStatus(string $conversionJobId) : array
+    {
+        $response = $this->client->getJob(['Id' => $conversionJobId]);
+        if (!$response) {
+            throw new Exception('Get no response');
+        }
+
+        return $response->get('Job');
+    }
+
+    public function isConversionJobComplete(array $conversionJobStatus): bool
+    {
+        return (isset($conversionJobStatus['Status']) && $conversionJobStatus['Status'] === 'COMPLETE');
+    }
+
+    public function getTargetExtension(): string
+    {
+        return ConversionSettingsType::EXTENSION;
+    }
+}
---
+++ app/Types/AmazonWebServices/ConversionSettingsType.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace App\Types\AmazonWebServices;
+
+class ConversionSettingsType
+{
+    const VIDEO_WIDTH = 1280;
+    const VIDEO_HEIGHT = 720;
+
+    /*
+     * Extension used for generating paths
+     *
+     * Make sure that extension relates to conversion settings container
+     */
+    const EXTENSION = 'mp4';
+
+    /*
+     * Default conversion settings for video conversion & thumbnail generation
+     *
+     * Some of the keys are as null, will be set later
+     */
+    protected array $conversionSettings = [
+        'OutputGroups' => [
+            /*
+             * Video
+             *
+             * If you change order of this OutputGroup then you should change index of it in
+             * $this->setVideosDestination() method
+             */
+            [
+                'Name' => 'Video',
+                'OutputGroupSettings' => [
+                    'Type' => 'FILE_GROUP_SETTINGS',
+                    'FileGroupSettings' => [
+                        'Destination' => null
+                    ]
+                ],
+                'Outputs' => [
+                    [
+                        'VideoDescription' => [
+                            'AfdSignaling' => 'NONE',
+                            'AntiAlias' => 'ENABLED',
+                            'CodecSettings' => [
+                                'Codec' => 'H_264',
+                                'H264Settings' => [
+                                    'AdaptiveQuantization' => 'HIGH',
+                                    'Bitrate' => 2200000,
+                                    'CodecLevel' => 'LEVEL_3_1',
+                                    'CodecProfile' => 'MAIN',
+                                    'EntropyEncoding' => 'CAVLC',
+                                    'FlickerAdaptiveQuantization' => 'DISABLED',
+                                    'FramerateControl' => 'SPECIFIED',
+                                    'FramerateConversionAlgorithm' => 'DUPLICATE_DROP',
+                                    'FramerateDenominator' => 1,
+                                    'FramerateNumerator' => 30,
+                                    'GopBReference' => 'DISABLED',
+                                    'GopClosedCadence' => 1,
+                                    'GopSize' => 90,
+                                    'GopSizeUnits' => 'FRAMES',
+                                    'HrdBufferInitialFillPercentage' => 90,
+                                    'InterlaceMode' => 'PROGRESSIVE',
+                                    'MinIInterval' => 0,
+                                    'NumberBFramesBetweenReferenceFrames' => 0,
+                                    'NumberReferenceFrames' => 3,
+                                    'ParControl' => 'INITIALIZE_FROM_SOURCE',
+                                    'QualityTuningLevel' => 'SINGLE_PASS',
+                                    'RateControlMode' => 'CBR',
+                                    'RepeatPps' => 'DISABLED',
+                                    'SceneChangeDetect' => 'ENABLED',
+                                    'Slices' => 1,
+                                    'SlowPal' => 'DISABLED',
+                                    'Softness' => 0,
+                                    'SpatialAdaptiveQuantization' => 'ENABLED',
+                                    'Syntax' => 'DEFAULT',
+                                    'Telecine' => 'NONE',
+                                    'TemporalAdaptiveQuantization' => 'ENABLED',
+                                    'UnregisteredSeiTimecode' => 'DISABLED',
+                                ]
+                            ],
+                            'ColorMetadata' => 'INSERT',
+                            'Height' => self::VIDEO_HEIGHT,
+                            'RespondToAfd' => 'NONE',
+                            'ScalingBehavior' => 'DEFAULT',
+                            'Sharpness' => 100,
+                            'TimecodeInsertion' => 'DISABLED',
+                            'Width' => self::VIDEO_WIDTH,
+                        ],
+                        'AudioDescriptions' => [
+                            [
+                                'AudioSourceName' => 'Audio Selector 1',
+                                'AudioTypeControl' => 'FOLLOW_INPUT',
+                                'CodecSettings' => [
+                                    'Codec' => 'AAC',
+                                    'AacSettings' => [
+                                        'AudioDescriptionBroadcasterMix' => 'NORMAL',
+                                        'Bitrate' => 128000,
+                                        'CodecProfile' => 'LC',
+                                        'CodingMode' => 'CODING_MODE_2_0',
+                                        'RateControlMode' => 'CBR',
+                                        'RawFormat' => 'NONE',
+                                        'SampleRate' => 44100,
+                                        'Specification' => 'MPEG4',
+                                    ]
+                                ],
+                                'LanguageCodeControl' => 'FOLLOW_INPUT',
+                            ]
+                        ],
+                        'ContainerSettings' => [
+                            'Container' => 'MP4',
+                            'Mp4Settings' => [
+                                'CslgAtom' => 'INCLUDE',
+                                'FreeSpaceBox' => 'EXCLUDE',
+                                'MoovPlacement' => 'PROGRESSIVE_DOWNLOAD'
+                            ]
+                        ],
+                    ],
+                ]
+            ],
+
+            /*
+             * Thumbnail
+             *
+             * If you change order of this OutputGroup then you should change index of it in
+             * $this->setThumbnailsDestination() method
+             */
+            [
+                'Name' => 'Thumbnail',
+                'OutputGroupSettings' => [
+                    'Type' => 'FILE_GROUP_SETTINGS',
+                    'FileGroupSettings' => [
+                        'Destination' => null
+                    ]
+                ],
+                'Outputs' => [
+                    [
+                        'VideoDescription' => [
+                            'CodecSettings' => [
+                                'Codec' => 'FRAME_CAPTURE',
+                                'FrameCaptureSettings' =>
+                                    [
+                                        // Frame capture will encode the first frame of the output stream, then one frame every framerateDenominator/framerateNumerator seconds.
+                                        'FramerateNumerator' => 1,
+                                        // Frame capture will encode the first frame of the output stream, then one frame every framerateDenominator/framerateNumerator seconds.
+                                        'FramerateDenominator' => 5,
+                                        /*
+                                         * By default MediaConvert will always store first frame of the video,
+                                         * and this cant be changed. So if we want also something else than (mostly)
+                                         * empty thumbnail then MaxCaptures should be at least 2.
+                                         *
+                                         */
+                                        'MaxCaptures' => 2,
+                                        'Quality' => 80,
+                                    ],
+                            ],
+                            'DropFrameTimecode' => 'ENABLED',
+                            'RespondToAfd' => 'NONE',
+                            'Sharpness' => 50,
+                            'AntiAlias' => 'ENABLED',
+                            'AfdSignaling' => 'NONE',
+                            'Width' => 1280,
+                            'ScalingBehavior' => 'DEFAULT',
+                            'ColorMetadata' => 'INSERT',
+                            'Height' => 720,
+                            'TimecodeInsertion' => 'DISABLED',
+                        ],
+                        'ContainerSettings' => [
+                            'Container' => 'RAW',
+                        ],
+                    ]
+                ]
+            ]
+        ],
+        'AdAvailOffset' => 0,
+        'Inputs' => [
+            [
+                'AudioSelectors' => [
+                    'Audio Selector 1' => [
+                        'Offset' => 0,
+                        'DefaultSelection' => 'NOT_DEFAULT',
+                        'ProgramSelection' => 1,
+                        'SelectorType' => 'TRACK'
+                    ]
+                ],
+                'VideoSelector' => [
+                    'ColorSpace' => 'FOLLOW'
+                ],
+                'FilterEnable' => 'AUTO',
+                'PsiControl' => 'USE_PSI',
+                'FilterStrength' => 0,
+                'DeblockFilter' => 'DISABLED',
+                'DenoiseFilter' => 'DISABLED',
+                'TimecodeSource' => 'EMBEDDED',
+                'FileInput' => null
+            ]
+        ],
+        'TimecodeConfig' => [
+            'Source' => 'EMBEDDED'
+        ]
+    ];
+
+    public function __construct(string $videoInputFullyQualifiedPath, string $videoOutputFullyQualifiedPath, string $videoThumbnailsFullyQualifiedPath)
+    {
+        // example: s3://input-bucket/source.mkv
+        $this->conversionSettings['Inputs'][0]['FileInput'] = $videoInputFullyQualifiedPath;
+
+        // example: s3://thumb-output-bucket/target - extension will be added automatically
+        $this->conversionSettings['OutputGroups'][0]['OutputGroupSettings']['FileGroupSettings']['Destination'] = $videoOutputFullyQualifiedPath;
+
+        // example: s3://thumb-output-bucket/folder-for-thumbnails/
+        $this->conversionSettings['OutputGroups'][1]['OutputGroupSettings']['FileGroupSettings']['Destination'] = $videoThumbnailsFullyQualifiedPath;
+    }
+
+    public function getAll() : array
+    {
+        return $this->conversionSettings;
+    }
+}
