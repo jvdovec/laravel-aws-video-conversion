38ca1796b282eea188bc1945daf2820366a1c0b9
Matej Jellus <juffalow@juffalow.com>
2020-09-10 13:58:19 +0200

    Update MediaConversionServiceInterface.php

--- app/Services/MediaConversionServiceInterface.php
+++ app/Services/MediaConversionServiceInterface.php
@@ -4,10 +4,29 @@ namespace App\Services;
 
 interface MediaConversionServiceInterface
 {
-    public function queueConversion(string $videoInputFullyQualifiedPathWithExtension, string $videoOutputFullyQualifiedPathWithoutExtension, string $videoThumbnailsFullyQualifiedPath) : string;
+    /**
+     *
+     * @param string $videoInputFullyQualifiedPathWithExtension
+     * @param string $videoOutputFullyQualifiedPathWithoutExtension
+     * @param string $videoThumbnailsFullyQualifiedPath
+     * @return string
+     */
+    public function queueConversion(
+        string $videoInputFullyQualifiedPathWithExtension,
+        string $videoOutputFullyQualifiedPathWithoutExtension,
+        string $videoThumbnailsFullyQualifiedPath
+    ) : string;
 
+    /**
+     *
+     * @param string $conversionJobId
+     */
     public function getConversionJobStatus(string $conversionJobId) : array;
 
+    /**
+     *
+     * @param array $conversionJobStatus
+     */
     public function isConversionJobComplete(array $conversionJobStatus) : bool;
 
     public function getTargetExtension() : string;
