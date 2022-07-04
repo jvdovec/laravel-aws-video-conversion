9cfa22a5e11ba88c29ed0a4cd6c19fda54cfddfb
Ján Vdovec <jan.vdovec@gmail.com>
2022-07-04 19:43:35 +0200

    Revert "Shift core files"
    This reverts commit 2c0a10530d15fdb5dc66499263d64699fc239953.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,12 +8,12 @@ use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
+     * The policy mappings for the application.
      *
-     * @var array<class-string, class-string>
+     * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
