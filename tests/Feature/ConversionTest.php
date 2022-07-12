371b6f40108a34546a7c38fe191588a68ed29a9d
Ján Vdovec <jan.vdovec@gmail.com>
2022-07-13 01:47:49 +0200

    Add testConversionShouldNotStartWithoutFile

--- tests/Feature/ConversionTest.php
+++ tests/Feature/ConversionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class ConversionTest extends TestCase
@@ -13,5 +14,12 @@ class ConversionTest extends TestCase
         $response->assertOk();
 
         $response->assertSeeText('Do Conversion');
+    }
+
+    public function testConversionShouldNotStartWithoutFile()
+    {
+        $response = $this->post('do-conversion');
+
+        $response->assertSeeText('The file field is required');
     }
 }
