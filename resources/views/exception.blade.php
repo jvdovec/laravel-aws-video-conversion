261288d20f25534fd7ab1e2013b5c195d5db0127
Ján Vdovec <jan.vdovec@gmail.com>
2022-07-13 01:31:03 +0200

    Add "Back to home" button

--- resources/views/exception.blade.php
+++ resources/views/exception.blade.php
@@ -12,4 +12,10 @@
             print_r($exception)
         @endphp
     </pre>
+
+    <div>
+        <a href="{{ route('show-upload-form') }}">
+            <button type="button" class="btn btn-warning mt-2">Back to home</button>
+        </a>
+    </div>
 @endsection
